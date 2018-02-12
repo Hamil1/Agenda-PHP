15e931bf6a8a80cbf1547874f01fcb50c7a1c47b
hamil <hamil@hamil-PC>
2018-02-12 11:03:52 -0400

    / incluyendo los archivos correctos de bootstrap y jquery

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!doctype html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Agenda - Index</title>
+        <script src="sweetalert-master/dist/sweetalert.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/Script.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0\css\font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/sweetalert-master/dist/sweetalert.css">
+        <link rel="stylesheet" type="text/css" href="css/Style.css">
     </head>
     <body>
         <?php require_once 'controladores/contactosControlador.php';
--- vistas/gestionarVista.php
+++ vistas/gestionarVista.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <!-- Latest compiled and minified CSS -->
-
-
-        <script src="sweetalert-master/dist/sweetalert.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="js/Script.js"></script>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0\css\font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="css/sweetalert-master/dist/sweetalert.css">
-        <link rel="stylesheet" type="text/css" href="css/Style.css">
         <?php 
             require_once 'modelos/contactosModelo.php';
             require_once 'vistas/agregarVista.php';
